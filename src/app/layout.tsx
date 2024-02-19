ff59c7d2ef5dc10393bb4d64bf826d9c044734e3
Herkamal <herkamalrandhawa@gmail.com>
2024-02-19 14:27:38 -0500

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <Notification/>
         <NavBar/>
-        <Footer/>
         {children}</body>
+        <Footer/>
     </html>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,42 @@
-import React from 'react'
-import Menu from './Menu'
-import Link from 'next/link'
+import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 const NavBar = () => {
+  const user = false;
   return (
-    <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase'>
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24">
+      {/* left links */}
+      <div className="hidden md:flex gap-4 ">
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
       {/* logo */}
-      <div className='text-xl'>
-        <Link href="/">
-        Massimo
-        </Link>
+      <div className="text-xl md:font-bold">
+        <Link href="/">Massimo</Link>
       </div>
       {/* Mobile */}
-      <div>
-        <Menu/>
+      <div className="md:hidden">
+        <Menu />
+      </div>
+      {/* right links */}
+      <div className="hidden md:flex gap-4 origin-center ">
+        <div className="flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+          <Image src="/phone.png" alt="" width={20} height={20}/>
+          <span> 123-456-7890</span>
+        </div>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/order">Order</Link>
+        )}
+        <CartIcon/>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
